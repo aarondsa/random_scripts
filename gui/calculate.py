1088ef846b0a3c3f0d818ab5804fbc20942a5c98
Aaron <aaron@aaron-Studio-1558.(none)>
2012-06-27 09:16:14 +0100

    add this calculating script...getting the gui to do something

---
+++ gui/calculate.py
@@ -0,0 +1,33 @@
+#script to figure out how tkinter works
+
+#this imports all the Tkinter stuff
+from Tkinter import *
+
+#create class for application
+class stuff:
+
+    def __init__(self, toplevel):
+        
+        self.frame = Frame(toplevel)
+        self.frame.pack()
+        
+        #the command= argument says execute the function self.multiply when the button is clicked
+        self.button = Button(self.frame, text='multiply', command=self.multiply)
+        self.button.pack()
+        
+        self.label = Label(self.frame, text='label')
+        self.label.pack()
+
+    #this function performs a simple multiplication and prints the result
+    def multiply(self):
+        a = 3
+        b = 4
+        result = a * b
+        print result
+
+#call tk() by assigning it to a variable. You have to call tk() at the beginning. 
+root = Tk()
+#call the application class. the argument is tk(), so that it is drawn in a window
+app = stuff(root)
+#keep running and waiting for user input
+root.mainloop()
